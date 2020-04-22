fe4af777295c613972b0e4a780cdea0e52fed215
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-22 23:23:33 +0200

    File Javascript con Commenti Ordinati

--- main.js
+++ main.js
@@ -4,31 +4,28 @@
 // al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
 //-----------------------------------------------------------------------------------------
 
-// multipli di 3
-
 
 for (var i = 1; i < 101; i++) {
 
+    // Calcolare i numeri che sono multipli di 3
     var multipli_3 = (i % 3 == 0);
+    // Calcolare i numeri che sono multipli di 5
     var multipli_5 = (i % 5 == 0);
 
-    // Calcolare i numeri che sono multipli di 3
+        // Se multiplo sia di 3 che di 5 scrivere la parola 'FizzBuzz' al posto del numero multiplo di 3 e di 5
     if (multipli_3 && multipli_5) {
-        // Se multiplo di 3 scrivere la parola 'Fizz' al posto del numero multiplo di 3
         console.log('Fizz' + 'Buzz');
 
-    // Calcolare i numeri che sono multipli di 5
+        // Se multiplo di 3 scrivere la parola 'Fizz' al posto del numero multiplo di 3
     } else if (multipli_3) {
-        // Se multiplo sia di 3 che di 5 scrivere la parola 'FizzBuzz' al posto del numero multiplo di 3 e di 5
         console.log('Fizz');
 
-    } else if (multipli_5) {
         // Se multiplo di 5 scrivere la parola 'Buzz' al posto del numero multiplo di 5
+    } else if (multipli_5) {
         console.log('Buzz');
 
-    // Calcolare i numeri che sono multipli di 3 e di 5
-    } else {
         // Scrivere numeri da 0 a 100 in sequenza, tranne i multipli di 3 e di 5
+    } else {
         console.log(i);
     }
 }
