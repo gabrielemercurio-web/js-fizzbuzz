c101cbebef0a16fbfb34e8e534d014cefea538b0
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-22 18:53:42 +0200

    Primo Tentativo (Non Riuscito)

---
+++ main.js
@@ -0,0 +1,21 @@
+// Scrivi un programma che stampi i numeri da 1 a 100
+for (var i = 0; i < 101; i++) {
+    console.log([i]);
+//Calcorare i multipli di 3
+    if ([i] % 3 == 0) {
+        console.log('Fizz');
+    } else if ([i] % 5 == 0) {
+        console.log('Buzz');
+    } else if (([i] % 3 == 0) && ([i] % 5 == 0)) {
+        console.log('FizzBuzz')
+    }
+}
+
+//Calcorare i multipli di 5
+
+
+//Calcorare i multipli di 3 e di 5
+
+// stampare "Fizz"
+// stampare "Buzz"
+// stampare "FizzBuzz"
