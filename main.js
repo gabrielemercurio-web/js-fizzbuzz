41188842d142d4e92f99d98fc7a873f4d759cf9e
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-22 21:51:33 +0200

    (Ricominciamo)  Percorso Logico Dei Comandi

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-var fizz = false;
-var buzz = false;
-var fizzbuzz = false;
+// Scrivi un programma che stampi i numeri da 1 a 100, con le seguenti regole:
+// al posto dei multipli di 3 stampi "Fizz"
+// al posto dei multipli di 5 stampi "Buzz"
+// al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
+//-----------------------------------------------------------------------------------------
 
-//Calcorare i multipli di 3
-var multiplo_3 = ([i] % 3 == 0);
+// Scrivere numeri da 0 a 100 in sequenza
+// Calcolare i numeri che sono multipli di 3
+// Calcolare i numeri che sono multipli di 5
+// Calcolare i numeri che sono multipli di 3 e di 5
 
-//Calcorare i multipli di 5
-var multiplo_5 = ([i] % 5 == 0);
-
-// Scrivi un programma che stampi i numeri da 1 a 100
-for (var i = 0; i < 101; i++) {
-    console.log([i]);
-
-    if ([i] == multiplo_3) {
-        fizz = true;
-        console.log([i] = 'Fizz');
-    }
-    else if ([i] == multiplo_5) {
-        buzz = true;
-        console.log([i] = 'Buzz');
-
-    } else if (([i] == multiplo_3) && ([i] == multiplo_5)) {
-        fizzbuzz = true;
-        console.log([i] = 'FizzBuzz');
-    }
-}
+// Se multiplo di 3 scrivere la parola 'Fizz' al posto del numero multiplo di 3
+// Se multiplo di 5 scrivere la parola 'Buzz' al posto del numero multiplo di 5
+// Se multiplo sia di 3 che di 5 scrivere la parola 'FizzBuzz' al posto del numero multiplo di 3 e di 5
