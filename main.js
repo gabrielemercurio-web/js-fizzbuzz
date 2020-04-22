1a1e28fb7822f1099d1d52ee96a374815d651354
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-04-22 19:35:11 +0200

    Secondo Tentativo (Fallito)

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
+var fizz = false;
+var buzz = false;
+var fizzbuzz = false;
+
+//Calcorare i multipli di 3
+var multiplo_3 = ([i] % 3 == 0);
+
+//Calcorare i multipli di 5
+var multiplo_5 = ([i] % 5 == 0);
+
 // Scrivi un programma che stampi i numeri da 1 a 100
 for (var i = 0; i < 101; i++) {
     console.log([i]);
-//Calcorare i multipli di 3
-    if ([i] % 3 == 0) {
-        console.log('Fizz');
-    } else if ([i] % 5 == 0) {
-        console.log('Buzz');
-    } else if (([i] % 3 == 0) && ([i] % 5 == 0)) {
-        console.log('FizzBuzz')
+
+    if ([i] == multiplo_3) {
+        fizz = true;
+        console.log([i] = 'Fizz');
+    }
+    else if ([i] == multiplo_5) {
+        buzz = true;
+        console.log([i] = 'Buzz');
+
+    } else if (([i] == multiplo_3) && ([i] == multiplo_5)) {
+        fizzbuzz = true;
+        console.log([i] = 'FizzBuzz');
     }
 }
-
-//Calcorare i multipli di 5
-
-
-//Calcorare i multipli di 3 e di 5
-
-// stampare "Fizz"
-// stampare "Buzz"
-// stampare "FizzBuzz"
